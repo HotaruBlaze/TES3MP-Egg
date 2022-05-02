7ec58d95705a6d9df702661c2acde40bbc78e819
Phoenix <DeriousHD@gmail.com>
2022-05-02 01:42:43 +0100

    Update to 0.8.1

--- Install_script.sh
+++ Install_script.sh
@@ -6,7 +6,7 @@ apt-get install -y curl jq
 useradd -ms /bin/bash container
 chown container:nogroup /home/container
 cd /home/container
-DOWNLOAD_URL=$(curl -sL https://api.github.com/repos/TES3MP/openmw-tes3mp/releases/tags/tes3mp-0.8.0 | jq -r ".assets[] | select(.name | contains(\"tes3mp-server-GNU+Linux-x86_64\")) | .browser_download_url")
+DOWNLOAD_URL=$(curl -sL https://api.github.com/repos/TES3MP/openmw-tes3mp/releases/tags/tes3mp-0.8.1 | jq -r ".assets[] | select(.name | contains(\"tes3mp-server-GNU+Linux-x86_64\")) | .browser_download_url")
 curl -O -J -L $DOWNLOAD_URL
 tar -xzvf *.tar.gz
 rm *.tar.gz
